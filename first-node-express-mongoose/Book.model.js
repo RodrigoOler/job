68ad411898f4a8dd754225f95f5000cc6280e3f5
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-12-08 22:39:02 -0200

    Refactoring and new implementations

---
+++ first-node-express-mongoose/Book.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+  title: String,
+  author: String,
+  category: String
+});
+
+module.exports = mongoose.model('Book', BookSchema);
--- first-node-express-mongoose/app.js
+++ first-node-express-mongoose/app.js
@@ -1,13 +1,20 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Book = require('./book.model')
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    Book = require('./Book.model'),
+    app = express(),
+    db = 'mongodb://localhost/example'
 
-const app = express();
+mongoose.connect(db);
+
 const PORT = 3000;
 
+// app.get('/', () => {
+//   res.send('Happy to be here');
+// });
+
 app.listen(PORT, () => {
-  console.log(`App Listening on PORT: ${PORT}`);
-})
+    console.log(`App Listening on PORT: ${PORT}`);
+});
